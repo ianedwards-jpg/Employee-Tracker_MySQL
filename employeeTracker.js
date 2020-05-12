c71d2b98d605f2d1b124aec08536bda6535c5efb
Ian Edwards <57515741+ianedwards-jpg@users.noreply.github.com>
2020-05-12 00:47:03 -0400

    Update employeeTracker.js
    Added cases for all option inquiries
    wrote employeeview function

--- employeeTracker.js
+++ employeeTracker.js
@@ -15,7 +15,7 @@ var connection = mysql.createConnection({
   database: "employeeProfiles_DB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   runSearch();
 });
@@ -31,36 +31,57 @@ function runSearch() {
         "View All Employees By Department",
         "View All Employees By Manager",
         "Add Employee",
-        "Remove Employee", 
+        "Remove Employee",
         "Update Employee Manager",
         "Update Employee Role",
         "View All Roles"
       ]
     })
-    .then(function(answer) {
+    .then(function (answer) {
       switch (answer.action) {
-      case "Find songs by artist":
-        artistSearch();
-        break;
+        case "View All Employees":
+          employeeView();
+          break;
 
-      case "Find all artists who appear more than once":
-        multiSearch();
-        break;
+        case "View All Employees By Department":
+          multiSearch();
+          break;
 
-      case "Find data within a specific range":
-        rangeSearch();
-        break;
+        case "View All Employees By Manager":
+          rangeSearch();
+          break;
 
-      case "Search for a specific song":
-        songSearch();
-        break;
+        case "Add Employee":
+          songSearch();
+          break;
 
-      case "Find artists with a top song and top album in the same year":
-        songAndAlbumSearch();
-        break;
+        case "Remove Employee":
+          songAndAlbumSearch();
+          break;
+
+        case "Update Employee Manager":
+          songAndAlbumSearch();
+          break;
+
+        case "Update Employee Role":
+          songAndAlbumSearch();
+          break;
+
+        case "View All Roles":
+          songAndAlbumSearch();
+          break;
       }
     });
 }
+
+function employeeView() {
+  var query = "SELECT * FROM employeeprofiles_db.employeedata;";
+  connection.query(query, function (err, res) {
+    console.log(res);
+  });
+  runSearch();
+}
+
 
 function artistSearch() {
   inquirer
@@ -69,25 +90,15 @@ function artistSearch() {
       type: "input",
       message: "What artist would you like to search for?"
     })
-    .then(function(answer) {
+    .then(function (answer) {
       var query = "SELECT position, song, year FROM top5000 WHERE ?";
-      connection.query(query, { artist: answer.artist }, function(err, res) {
+      connection.query(query, { artist: answer.artist }, function (err, res) {
         for (var i = 0; i < res.length; i++) {
           console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
         }
         runSearch();
       });
     });
-}
-
-function multiSearch() {
-  var query = "SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1";
-  connection.query(query, function(err, res) {
-    for (var i = 0; i < res.length; i++) {
-      console.log(res[i].artist);
-    }
-    runSearch();
-  });
 }
 
 function rangeSearch() {
@@ -97,7 +108,7 @@ function rangeSearch() {
         name: "start",
         type: "input",
         message: "Enter starting position: ",
-        validate: function(value) {
+        validate: function (value) {
           if (isNaN(value) === false) {
             return true;
           }
@@ -108,7 +119,7 @@ function rangeSearch() {
         name: "end",
         type: "input",
         message: "Enter ending position: ",
-        validate: function(value) {
+        validate: function (value) {
           if (isNaN(value) === false) {
             return true;
           }
@@ -116,19 +127,19 @@ function rangeSearch() {
         }
       }
     ])
-    .then(function(answer) {
+    .then(function (answer) {
       var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
-      connection.query(query, [answer.start, answer.end], function(err, res) {
+      connection.query(query, [answer.start, answer.end], function (err, res) {
         for (var i = 0; i < res.length; i++) {
           console.log(
             "Position: " +
-              res[i].position +
-              " || Song: " +
-              res[i].song +
-              " || Artist: " +
-              res[i].artist +
-              " || Year: " +
-              res[i].year
+            res[i].position +
+            " || Song: " +
+            res[i].song +
+            " || Artist: " +
+            res[i].artist +
+            " || Year: " +
+            res[i].year
           );
         }
         runSearch();
@@ -143,18 +154,18 @@ function songSearch() {
       type: "input",
       message: "What song would you like to look for?"
     })
-    .then(function(answer) {
+    .then(function (answer) {
       console.log(answer.song);
-      connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function(err, res) {
+      connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function (err, res) {
         console.log(
           "Position: " +
-            res[0].position +
-            " || Song: " +
-            res[0].song +
-            " || Artist: " +
-            res[0].artist +
-            " || Year: " +
-            res[0].year
+          res[0].position +
+          " || Song: " +
+          res[0].song +
+          " || Artist: " +
+          res[0].artist +
+          " || Year: " +
+          res[0].year
         );
         runSearch();
       });
@@ -168,26 +179,26 @@ function songAndAlbumSearch() {
       type: "input",
       message: "What artist would you like to search for?"
     })
-    .then(function(answer) {
+    .then(function (answer) {
       var query = "SELECT top_albums.year, top_albums.album, top_albums.position, top5000.song, top5000.artist ";
       query += "FROM top_albums INNER JOIN top5000 ON (top_albums.artist = top5000.artist AND top_albums.year ";
       query += "= top5000.year) WHERE (top_albums.artist = ? AND top5000.artist = ?) ORDER BY top_albums.year, top_albums.position";
 
-      connection.query(query, [answer.artist, answer.artist], function(err, res) {
+      connection.query(query, [answer.artist, answer.artist], function (err, res) {
         console.log(res.length + " matches found!");
         for (var i = 0; i < res.length; i++) {
           console.log(
-            i+1 + ".) " +
-              "Year: " +
-              res[i].year +
-              " Album Position: " +
-              res[i].position +
-              " || Artist: " +
-              res[i].artist +
-              " || Song: " +
-              res[i].song +
-              " || Album: " +
-              res[i].album
+            i + 1 + ".) " +
+            "Year: " +
+            res[i].year +
+            " Album Position: " +
+            res[i].position +
+            " || Artist: " +
+            res[i].artist +
+            " || Song: " +
+            res[i].song +
+            " || Album: " +
+            res[i].album
           );
         }
 
