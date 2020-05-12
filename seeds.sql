440e865b26200a9a0bd9ae264473f275c9e4b7a7
Ian Edwards <57515741+ianedwards-jpg@users.noreply.github.com>
2020-05-12 00:00:19 -0400

    further work seeds and schema

--- seeds.sql
+++ seeds.sql
@@ -1,6 +1,12 @@
-INSERT INTO employeeData (first_name, last_name, role_id, manager_id) VALUES ('Meeses');
-INSERT INTO employeeData (first_name, last_name, role_id, manager_id) VALUES ('Bobbi');
-INSERT INTO employeeData (first_name, last_name, role_id, manager_id) VALUES ('Sylvester', true);
-INSERT INTO employeeData (first_name, last_name, role_id, manager_id) VALUES ('Marilyn Manson', true);
-INSERT INTO employeeData (first_name, last_name, role_id, manager_id) VALUES ('Joe Biden', true);
-INSERT INTO employeeData (first_name, last_name, role_id, manager_id) VALUES ('Owens');
+INSERT INTO employeeData (first_name, last_name, role_id, manager_id) VALUES ('Meeses', 'Bodnar', '11210', '561646' );
+INSERT INTO employeeData (first_name, last_name, role_id, manager_id) VALUES ('Kayla', 'Franco', '280456', '561646' );
+INSERT INTO employeeData (first_name, last_name, role_id, manager_id) VALUES ('Dave', 'Kazakh', '201456', '561646' );
+INSERT INTO employeeData (first_name, last_name, role_id, manager_id) VALUES ('Thesus', 'Logie', '280456', '561646' );
+INSERT INTO employeeData (first_name, last_name, role_id, manager_id) VALUES ('Abe', 'Lincoln', '201456', '561646' );
+INSERT INTO employeeData (first_name, last_name, role_id, manager_id) VALUES ('Joe', 'Biden', '280456', '561646' );
+
+INSERT INTO roleData (title, salary, role_id, department_id) VALUES ('Engineer', '75000', '280456', '561646' );
+INSERT INTO roleData (title, salary, role_id, department_id) VALUES ('Intern', '25000', '201456', '561646' );
+INSERT INTO roleData (title, salary, role_id, department_id) VALUES ('Manager', '275000', '11210', '561646' );
+
+INSERT INTO departmentData (department_name) VALUES ('Meeses', 'Bodnar', '11210', '561646' );
